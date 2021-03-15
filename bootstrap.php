f2a26b0338cc3e91dfcc49a60787e8247aca8755
Sony George <sony@thinkberries.com>
2021-03-15 17:45:56 +0530

    First Push

---
+++ bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+$path = __DIR__ . "/";
+
+require $path. 'vendor/autoload.php';
+
+use Aws\Kinesis\KinesisClient; 
+use Aws\Exception\AwsException;
+
+$kinesisClient = new Aws\Kinesis\KinesisClient([
+    'profile' => 'localstack',
+    'version' => '2013-12-02',
+    'region' => 'us-east-1'
+]);
+
+
+function stop($var, $e=1) {
+    echo "<pre>";
+    print_r($var);
+    echo "</pre>";
+    if ($e) exit;
+}
+
+$write = function ($data) use ($kinesisClient) {
+    $encoded_data = json_encode($data);
+    stop($encoded_data);
+};
